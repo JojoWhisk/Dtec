85d978ea5dac2ea7ee154f1cf701442eb9533a02
Jojo <joao.asn09@gmail.com>
2025-09-10 11:15:10 -0300

    update localhost buscar nome e id - postman

--- MY API/server.js
+++ MY API/server.js
@@ -12,13 +12,43 @@ const PORT = 3001;
 
 const usuarios = [
     {id: 1, nome: "Ana", idade: 25},
-    {id: 2, nome: "Carlos", idade: 25},
-    {id: 3, nome: "Maria", idade: 25}
+    {id: 2, nome: "Carlos", idade: 30},
+    {id: 3, nome: "Maria", idade: 22},
+    {id: 4, nome: "João Carlos", idade: 42}
 ]
 
-app.get('/usuarios', (req,res) => {
-    res.json(usuarios)
+app.get('/',(req,res) => {
+    res.send("Página Inicial")
 })
+
+app.get('/usuarios',(req,res) => {
+    res.json(usuarios);
+})
+
+app.get('/usuarios/:id', (req, res) => {
+    const id = req.params.id
+    const usuario = usuarios.find(u => u.id == id)
+    
+    if(usuario){
+        res.json(usuario)
+    }else{
+        res.status(404).json({mensagem: 'Usuário não encontrado'})
+    }
+})
+
+app.get('/usuarios/buscar/:nome', (req,res) => {
+    const buscaNome = req.params.nome.toLowerCase()
+    const resultados = usuarios.filter(u => u.nome.toLowerCase().includes(buscaNome))
+
+    if(resultados.length > 0){
+        res.json(resultados)
+    }else{
+        res.status(404).json({mensagem: 'Usuário não encontrado'})
+    }
+})
+
+
+
 
 //Inicia o servidor 
 app.listen(PORT, () => {
